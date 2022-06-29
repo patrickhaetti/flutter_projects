96f9bd1849a82ae3b4ed3bcb0756e545f5ee7d6a
Patrick <patrich.haetti@gmail.com>
2022-06-29 17:47:29 +0200

    added column app

---
+++ columns/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello test app'),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(199, 80, 66, 202),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Text('Hello'),
+              Text('Wolrd'),
+            ],
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            color: Colors.cyan,
+            child: Text('one'),
+          ),
+          Container(
+            padding: EdgeInsets.all(30.0),
+            color: Colors.red,
+            child: Text('two'),
+          ),
+          Container(
+            padding: EdgeInsets.all(40.0),
+            color: Color.fromARGB(255, 52, 145, 24),
+            child: Text('three'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Click'),
+        backgroundColor: Color.fromARGB(100, 143, 0, 123),
+      ),
+    );
+  }
+}
